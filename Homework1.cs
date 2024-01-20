92aa703bd2ed2b12760fabf04fc311c54eb5d76f
marshhalls <157026439+marshhalls@users.noreply.github.com>
2024-01-20 08:06:08 -0600

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*Q1 (5 Points) The linear line Equation is: Z = 4X2+3Y, when  X = 2.5, Y=3.3, write a program to:
+calculate the value of Z and print it out in the following format.
+(Hint: in computer programming,  4X2  can be represented as 4*X*X; and use formatted string to print results)*/
+int x = 2.5
+int y = 3.3
+int z = (4*x*x) + (3*y)
+Console.WriteLine(z);
+    }
+}
+
