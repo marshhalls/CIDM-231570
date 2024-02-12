0b3e7062cb7499f0580f0540bbb8bc8be96c4a61
marshhalls <157026439+marshhalls@users.noreply.github.com>
2024-02-11 19:34:15 -0600

    Add files via upload

---
+++ HW4.cs
@@ -0,0 +1,44 @@
+namespace HW;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Homework 4);
+        Console.WriteLine(Q1:)
+        int x=12, y=7;
+         if(x>y){
+        Console.WriteLine("The largest number is: x");
+        }
+        else{
+        Console.WriteLine("The largest number is: y");
+        }
+    }
+    {
+        Console.WriteLine(Q2:);
+        string Y= Console.ReadLine();
+        int Y= 5;
+        if{
+            string Y= Console.ReadLine();
+        int N= 5;
+        for(int row = 0; row<N; row++){
+            for(int col=0; col<N; col++)
+            {
+                if(row>=col)
+                Console.Write('*')
+            }
+            Console.WriteLine("N is: 5; shape is left")
+        else{
+            string Y= Console.ReadLine();
+        int N= 5;
+        for(int row = 0; row<N; row--){
+            for(int col=0; col<N; col--)
+            {
+                if(row>=col)
+                Console.Write('*')
+            }
+            Console.WriteLine("N is: 5; shape is right")
+        }
+            }
+    }
+
+}
