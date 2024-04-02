561f83eec2eea2c45a55436b11dfe8014e86da40
marshhalls <157026439+marshhalls@users.noreply.github.com>
2024-04-01 20:18:53 -0500

    Add files via upload

---
+++ HW9.cs
@@ -0,0 +1,61 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+    string[] name_arr1 = new string[3];
+    name_arr1[0] = "Alice";
+    name_arr1[1] = "Bob";
+    name_arr1[2] = "Cathy";
+    name_arr1[3] = "David";
+    public static studentList
+    string[] name_arr2 = new string[]{"Alice","Bob","Cathy","David","Tom"};
+    List<string> Student = new List<string>();
+    name_list1.Add("Alice");
+    name_list1.Add("Bob");
+    name_list1.Add("Cathy");
+    name_list1.Add("David");
+    name_list1.Add("Tom");
+    name_list1.Add(null);
+    }
+}
+using System.Linq;
+class Student
+{
+    private string studentName;
+    private int studentID;
+    public void PrintInfo(){
+    Console.WriteLine($"Student ID:{studentID}, Student Name:{studentName}");
+        }
+}
+    {
+    public Student(int studentID, string studentName, int studentGPA){
+    id = input studentID;
+    gpa = input studentGPA;
+    name = input studentName;
+    }
+    {
+        int[] int_gpa_array = {4.0,3.6,2.5,1.8,3.3};
+        MaxMinSum(int_gpa_array);
+    }
+    static void MaxMinSum(int[] int_gpa_array){
+    Console.WriteLine($"The average GPA is: {int_gpa_array.Average()}");
+    }
+
+    List<string> stu_info = new List<string>(){"Alice","Bob","Cathy","David","Tom",null};
+    Gradebook<int, string> stu_info = new Gradebook<int,string>();
+    stu_info.Add("Alice", 4.0);
+    stu_info.Add("Bob", 3.6);
+    stu_info.Add("Cathy",2.5);
+    stu_info.Add("David",1.8);
+    stu_info.Add("Tom",3.3);
+    foreach(var stu in stu_info){
+       Console.WriteLine($"Student ID: {studentID}, Student Name: {studentName}");
+    if(stu_info.ContainsKey(gpa>=int_gpa_array)){
+        stu_info.Remove(gpa<=int_gpa_array);
+    foreach(var stu in stu_info){
+        Console.WriteLine($"Student ID: {studentID}, Student Name: {studentName}");
+}
+        
+        }
+    }
+}
